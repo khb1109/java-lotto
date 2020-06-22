59da1dc37e9cc4355182a82fe2207556230fc5b9
hongbin23 <khbasd@gmail.com>
2020-06-22 22:37:33 +0900

    feat: inputview 클래스 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,39 @@
 package view;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class InputView {
+	private static final Scanner scanner = new Scanner(System.in);
+	private static final String DELIMETER = ", ";
+
+	public Long lottoMoney() {
+		System.out.println("구입금액을 입력해 주세요.");
+		return Long.valueOf(scanner.nextLine());
+	}
+
+	public int manualLottoAmount() {
+		System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+		return Integer.parseInt(scanner.nextLine());
+	}
+
+	public List<Integer> manualLotto() {
+		return inputLotto();
+	}
+
+	public List<Integer> winningLotto() {
+		return inputLotto();
+	}
+
+	public int bonusNumber() {
+		return Integer.parseInt(scanner.next());
+	}
+
+	private List<Integer> inputLotto() {
+		return Arrays.stream(scanner.nextLine().split(DELIMETER))
+			.map(Integer::parseInt)
+			.collect(Collectors.toList());
+	}
 }
